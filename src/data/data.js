ca4f998833ddb398733e1ab0e69e21bc549e4076
Sean Pheavyraksonya <sean.pheavyraksonya24@kit.edu.kh>
2026-03-30 02:15:33 +0700

    fix promo banner

--- src/data/data.js
+++ src/data/data.js
@@ -40,7 +40,7 @@ export const rule = [
 ];
 
 export const promo = [
-  { id: 1, image: "src/assets/banner/download-1.webp", btn: "BEST SELLER" },
-  { id: 2, image: "src/assets/banner/download-2.webp", btn: "GOODS/MERCH" },
-  { id: 3, image: "src/assets/banner/download-3.webp", btn: "GIFT CARD" },
+  { id: 1, image: "/assets/banner/download-1.webp", btn: "BEST SELLER" },
+  { id: 2, image: "/assets/banner/download-2.webp", btn: "GOODS/MERCH" },
+  { id: 3, image: "/assets/banner/download-3.webp", btn: "GIFT CARD" },
 ];
