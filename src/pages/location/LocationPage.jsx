de09ffba7d9a6313ecf6fdccc28657a74c44d297
Sean Pheavyraksonya <sean.pheavyraksonya24@kit.edu.kh>
2026-03-30 01:35:45 +0700

    fix import case in LocationPage

--- src/pages/location/LocationPage.jsx
+++ src/pages/location/LocationPage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Banner1 from "./components/banner1";
+import Banner1 from "./components/Banner1";
 import ListingShop from "./components/ListingShop";
 import Banner2 from "./components/Banner2";
 const LocationPage = () => {
